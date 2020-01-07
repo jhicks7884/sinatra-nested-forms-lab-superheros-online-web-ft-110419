51882c0c0a5f151d3d516f1d4bc086d530daca9f
Jeshua Hicks <jhicks7884@outlook.com>
2020-01-07 18:22:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,15 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-      erb :'../views/superhero'
-    end
-
-    post '/teams' do
-      @team_name = params["team"]["name"]
-      @team_motto = params["team"]["motto"]
-      @team_members = params["team"]["members"]
-
-      erb :'../views/team'
+       erb :'../views/super_hero'
+     end
+ 
+     post '/teams' do
+       @team_name = params["team"]["name"]
+       @team_motto = params["team"]["motto"]
+       @team_members = params["team"]["members"]
+ 
+       erb :'../views/team'
     end
 
 
